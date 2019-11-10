01b4fd4e70d0f773121d435da6e3a7e40a97cecc
MAZAKPE <makpalyy@gmail.com>
2019-11-10 16:25:59 +0200

    modified the bootcamp controller file

--- controllers/bootcamp.controller.js
+++ controllers/bootcamp.controller.js
@@ -38,7 +38,6 @@ module.exports = {
         if (req.query.select) {
             const fields = req.query.select.split(',').join(' ')
             query = query.select(fields)
-            console.log(fields)
         }
 
         //excecuting query
