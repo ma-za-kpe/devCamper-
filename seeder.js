c022f3ce79f1ea648bdfc22a793e0a645e47bdd2
MAZAKPE <makpalyy@gmail.com>
2019-11-10 20:21:00 +0200

    added coure model and modified seeder

--- seeder.js
+++ seeder.js
@@ -10,7 +10,7 @@ dotenv.config({
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
-// const Course = require('./models/Course');
+const Course = require('./models/Course');
 // const User = require('./models/User');
 // const Review = require('./models/Review');
 
@@ -29,9 +29,9 @@ const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
-// const courses = JSON.parse(
-//     fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
-// );
+const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 // const users = JSON.parse(
 //     fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
@@ -45,7 +45,7 @@ const bootcamps = JSON.parse(
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
-        // await Course.create(courses);
+        await Course.create(courses);
         // await User.create(users);
         // await Review.create(reviews);
         console.log('Data Imported...'.green.bold);
@@ -59,7 +59,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
-        // await Course.deleteMany();
+        await Course.deleteMany();
         // await User.deleteMany();
         // await Review.deleteMany();
         console.log('Data Destroyed...'.red.bold);
