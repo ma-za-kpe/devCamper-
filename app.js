ed0c9baf13022dbef66098e612d26b868e8d3864
MAZAKPE <makpalyy@gmail.com>
2019-11-30 08:19:27 +0200

    modified the bootcamp controller file

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ dotenv.config({
 var app = express();
 
 // Set static folder
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
