95758fa8690326883c621473343ccfdbffbf78ba
MAZAKPE <makpalyy@gmail.com>
2019-11-30 07:58:58 +0200

    modified the bootcamp controller file

--- controllers/bootcamp.controller.js
+++ controllers/bootcamp.controller.js
@@ -220,15 +220,15 @@ module.exports = {
             );
         }
 
-        // Make sure user is bootcamp owner
-        if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
-            return next(
-                new ErrorResponse(
-                    `User ${req.params.id} is not authorized to update this bootcamp`,
-                    401
-                )
-            );
-        }
+        // // Make sure user is bootcamp owner
+        // if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        //     return next(
+        //         new ErrorResponse(
+        //             `User ${req.params.id} is not authorized to update this bootcamp`,
+        //             401
+        //         )
+        //     );
+        // }
 
         if (!req.files) {
             return next(new ErrorResponse(`Please upload a file`, 400));
