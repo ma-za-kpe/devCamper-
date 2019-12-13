ac145112ecd010f68ba2199d83587e09e865287e
MAZAKPE <makpalyy@gmail.com>
2019-12-13 10:32:20 +0200

    added middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -3,6 +3,10 @@ var express = require('express');
 //other reaource routers
 const courseRouter = require('./courses');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
+
 var router = express.Router();
 
 //re-route into other resources routers
@@ -15,7 +19,7 @@ const BootcampController = require('../controllers/bootcamp.controller');
 router.route('/radius/:zipcode/:distance').get(BootcampController.getBootcampsInRadius);
 
 /* GET and POST  all bootcamp listing. */
-router.route('/').get(BootcampController.getAllBootcamps).post(BootcampController.createBootcamps);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(BootcampController.createBootcamps);
 
 /* GET bootcamp listing, UPDATE bootcamp listing, DELETE bootcamp listing. */
 router.route('/:id').get(BootcampController.getOneBootcamps).put(BootcampController.updateBootcamps).delete(BootcampController.deleteBootcamps);
