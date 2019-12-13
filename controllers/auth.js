3b93f4bdff585ab37ae1c61ff33bc9919199b197
MAZAKPE <makpalyy@gmail.com>
2019-12-13 14:47:14 +0200

    added middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 const ErrorResponse = require('../utils/errorResponse');
-const asyncHandler = require('../middleware/async');
+// const asyncHandler = require('../middleware/async');
 const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
