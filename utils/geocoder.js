bc7d6b878d42b292c8a017186370924112f29fc9
MAZAKPE <makpalyy@gmail.com>
2019-12-16 00:58:04 +0200

    modified register controller and reoute

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,13 +1,5 @@
 const NodeGeocoder = require('node-geocoder');
 
-const dotenv = require('dotenv');
-
-// Load env vars
-require('dotenv').config({
-    path: __dirname + '/.env',
-    silent: true
-})
-
 console.log("In geocoder...provider is ..." + process.env.GEOCODER_PROVIDER + "  key is ..." + process.env.GEOCODER_API_KEY)
 
 const options = {
