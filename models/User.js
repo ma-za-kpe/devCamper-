47ff0237ec8cb4143cf99b3cddf6be68e619c067
MAZAKPE <makpalyy@gmail.com>
2019-12-15 23:34:24 +0200

    modified register controller and reoute

--- models/User.js
+++ models/User.js
@@ -46,14 +46,14 @@ UserSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-// // Sign JWT and return
-// UserSchema.methods.getSignedJwtToken = function () {
-//     return jwt.sign({
-//         id: this._id
-//     }, process.env.JWT_SECRET, {
-//         expiresIn: process.env.JWT_EXPIRE
-//     });
-// };
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({
+    id: this._id //current users id
+  }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
 
 // // Match user entered password to hashed password in database
 // UserSchema.methods.matchPassword = async function (enteredPassword) {
