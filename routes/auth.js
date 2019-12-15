b4e160367fbf1699c0d4942e25fb7e2ba7fa463a
MAZAKPE <makpalyy@gmail.com>
2019-12-15 23:06:10 +0200

    testing register

--- routes/auth.js
+++ routes/auth.js
@@ -17,12 +17,12 @@ const {
 } = require('../middleware/auth');
 
 router.post('/register', register);
-router.post('/login', login);
-router.get('/logout', logout);
-router.get('/me', protect, getMe);
-router.put('/updatedetails', protect, updateDetails);
-router.put('/updatepassword', protect, updatePassword);
-router.post('/forgotpassword', forgotPassword);
-router.put('/resetpassword/:resettoken', resetPassword);
+// router.post('/login', login);
+// router.get('/logout', logout);
+// router.get('/me', protect, getMe);
+// router.put('/updatedetails', protect, updateDetails);
+// router.put('/updatepassword', protect, updatePassword);
+// router.post('/forgotpassword', forgotPassword);
+// router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
