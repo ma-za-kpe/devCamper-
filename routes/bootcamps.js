91e222006d0985a3f0b225a01d633994aec35a3b
MAZAKPE <makpalyy@gmail.com>
2019-12-16 00:28:33 +0200

    modified register controller and reoute

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,7 +5,7 @@ const courseRouter = require('./courses');
 
 const Bootcamp = require('../models/Bootcamp');
 const advancedResults = require('../middleware/advancedResults');
-
+const protect = require('../middleware/auth');
 
 var router = express.Router();
 
@@ -19,11 +19,11 @@ const BootcampController = require('../controllers/bootcamp.controller');
 router.route('/radius/:zipcode/:distance').get(BootcampController.getBootcampsInRadius);
 
 /* GET and POST  all bootcamp listing. */
-router.route('/').get(advancedResults(Bootcamp, 'courses'), BootcampController.getAllBootcamps).post(BootcampController.createBootcamps);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), BootcampController.getAllBootcamps).post(protect, BootcampController.createBootcamps);
 
 /* GET bootcamp listing, UPDATE bootcamp listing, DELETE bootcamp listing. */
-router.route('/:id').get(BootcampController.getOneBootcamps).put(BootcampController.updateBootcamps).delete(BootcampController.deleteBootcamps);
+router.route('/:id').get(BootcampController.getOneBootcamps).put(protect, BootcampController.updateBootcamps).delete(protect, BootcampController.deleteBootcamps);
 
 /* upload photo */
-router.route('/:id/photo').put(BootcampController.bootcampPhotoUpload);
+router.route('/:id/photo').put(protect, BootcampController.bootcampPhotoUpload);
 module.exports = router;
