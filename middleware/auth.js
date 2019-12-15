0e3c9223307632fa9796c4b533d45de05c02f9cd
MAZAKPE <makpalyy@gmail.com>
2019-12-16 00:39:55 +0200

    modified register controller and reoute

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,37 +4,41 @@ const ErrorResponse = require('../utils/errorResponse');
 const User = require('../models/User');
 
 // Protect routes
-module.exports.protect = asyncHandler(async (req, res, next) => {
-    let token;
+module.exports = {
 
-    if ( //checking or authorisation headers
-        req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
-    ) {
-        // Set token from Bearer token in header
-        token = req.headers.authorization.split(' ')[1];
-        // Set token from cookie
-    }
-    // else if (req.cookies.token) {
-    //   token = req.cookies.token;
-    // }
+    protect: asyncHandler(async (req, res, next) => {
+        let token;
 
-    // Make sure token exists
-    if (!token) {
-        return next(new ErrorResponse('Not authorized to access this route', 401));
-    }
+        if ( //checking or authorisation headers
+            req.headers.authorization &&
+            req.headers.authorization.startsWith('Bearer')
+        ) {
+            // Set token from Bearer token in header
+            token = req.headers.authorization.split(' ')[1];
+            // Set token from cookie
+        }
+        // else if (req.cookies.token) {
+        //   token = req.cookies.token;
+        // }
 
-    try {
-        // Verify token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log(decoded)
-        req.user = await User.findById(decoded.id);
+        // Make sure token exists
+        if (!token) {
+            return next(new ErrorResponse('Not authorized to access this route', 401));
+        }
 
-        next();
-    } catch (err) {
-        return next(new ErrorResponse('Not authorized to access this route', 401));
-    }
-});
+        try {
+            // Verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decoded)
+            req.user = await User.findById(decoded.id);
+
+            next();
+        } catch (err) {
+            return next(new ErrorResponse('Not authorized to access this route', 401));
+        }
+    })
+
+}
 
 // // Grant access to specific roles
 // exports.authorize = (...roles) => {
