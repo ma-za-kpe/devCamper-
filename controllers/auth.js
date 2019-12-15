232ab4cd93d8511f737df5a2bee2b9ecd46de3c1
MAZAKPE <makpalyy@gmail.com>
2019-12-16 00:13:31 +0200

    modified register controller and reoute

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,15 +25,7 @@ module.exports = {
             role
         });
 
-        // create token
-        const token = user.getSignedJwtToken();
-
-        res.status(200).json({
-            success: true,
-            token: token
-        });
-
-        // sendTokenResponse(user, 200, res);
+        sendTokenResponse(user, 200, res);
     }),
     // @desc      Login user
     // @route     POST /api/v1/auth/login
@@ -66,15 +58,33 @@ module.exports = {
             return next(new errorResponse('Invalid credentials', 401));
         }
 
-        // create token
-        const token = user.getSignedJwtToken();
-
-        res.status(200).json({
-            success: true,
-            token: token
-        });
-
-        // sendTokenResponse(user, 200, res);
+        sendTokenResponse(user, 200, res);
 
     })
+
+};
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    const options = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000 //30 days from now
+        ),
+        httpOnly: true
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true;
+    }
+
+    res
+        .status(statusCode)
+        .cookie('token', token, options)
+        .json({
+            success: true,
+            token
+        });
 };
