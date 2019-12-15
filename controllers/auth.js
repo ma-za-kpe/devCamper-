7d8a417410b5ab12b3adec92dca3bfbddd3d61b9
MAZAKPE <makpalyy@gmail.com>
2019-12-15 23:05:30 +0200

    testing register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,28 @@
-const crypto = require('crypto');
-const ErrorResponse = require('../utils/errorResponse');
+const crypto = require("crypto");
+const ErrorResponse = require("../utils/errorResponse");
 // const asyncHandler = require('../middleware/async');
-const sendEmail = require('../utils/sendEmail');
-const User = require('../models/User');
+const sendEmail = require("../utils/sendEmail");
+const User = require("../models/User");
 
 module.exports = {
-    // @desc      Register user
-    // @route     POST /api/v1/auth/register
-    // @access    Public
-    registerUser: asyncHandler(async (req, res, next) => {
-        const {
-            name,
-            email,
-            password,
-            role
-        } = req.body;
+  // @desc      Register user
+  // @route     POST /api/v1/auth/register
+  // @access    Public
+  registerUser: asyncHandler(async (req, res, next) => {
+    const { name, email, password, role } = req.body;
 
-        // Create user
-        const user = await User.create({
-            name,
-            email,
-            password,
-            role
-        });
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role
+    });
 
-        sendTokenResponse(user, 200, res);
-    }),
-}
+    res.status(200).json({
+      success: true
+    });
+
+    // sendTokenResponse(user, 200, res);
+  })
+};
