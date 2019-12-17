fedbfc2401435d3dd80bbec761802fedb01a4afd
MAZAKPE <makpalyy@gmail.com>
2019-12-17 13:18:30 +0200

    added bootcamp ownership

--- controllers/auth.js
+++ controllers/auth.js
@@ -152,6 +152,42 @@ module.exports = {
 
             return next(new errorResponse('Email could not be sent', 500));
         }
+    }),
+    // @desc      Update user details
+    // @route     PUT /api/v1/auth/updatedetails
+    // @access    Private
+    updateDetails: asyncHandler(async (req, res, next) => {
+        const fieldsToUpdate = {
+            name: req.body.name,
+            email: req.body.email
+        };
+
+        const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+            new: true,
+            runValidators: true
+        });
+
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    }),
+
+    // @desc      Update password
+    // @route     PUT /api/v1/auth/updatepassword
+    // @access    Private
+    updatePassword: asyncHandler(async (req, res, next) => {
+        const user = await User.findById(req.user.id).select('+password');
+
+        // Check current password
+        if (!(await user.matchPassword(req.body.currentPassword))) {
+            return next(new errorResponse('Password is incorrect', 401));
+        }
+
+        user.password = req.body.newPassword;
+        await user.save();
+
+        sendTokenResponse(user, 200, res);
     })
 
 };
